0dcbe2ab5b3e7c98b493cc8fe000677e459f68ac
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-06-07 17:00:14 +0330

    add users model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+});
+
+const model = mongoose.model.User
+  ? mongoose.model.User
+  : mongoose.model("User", schema);
+
+export default model;
