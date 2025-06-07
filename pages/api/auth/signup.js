f1c2d9f45df4809cadc4f4095549f5fda9048ee5
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-06-07 20:52:32 +0330

    add normal signup method

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,2 +1,37 @@
-const handler = (req, res) => {};
+import connecttoDB from "@/configs/db";
+import userModel from "@/models/User";
+
+const handler = async (req, res) => {
+  if (req.method !== "POST") {
+    return false;
+  }
+  try {
+    connecttoDB();
+
+    const { firstname, lastname, username, email, password } = req.body;
+
+    if (
+      !firstname.trim() ||
+      !lastname.trim() ||
+      !username.trim() ||
+      !email.trim() ||
+      !password.trim()
+    ) {
+      return res.status(422).json({ message: "Data is not valid" });
+    }
+
+    await userModel.create({
+      firstname,
+      lastname,
+      username,
+      email,
+      password,
+      role: "USER",
+    });
+
+    return res.status(201).json({ message: "User got created successfully" });
+  } catch (err) {
+    return res.status(500).json({ message: "Unkown internal server error" });
+  }
+};
 export default handler;
