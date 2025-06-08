a5c36d87d0ef6698dd73ee17807be39fb0d8a0e7
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-06-08 12:51:11 +0330

    get signup form values

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Index() {
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [userName, setUserName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   return (
     <div className="box">
       <h1 align="center">SignUp Form</h1>
       <form role="form" method="post">
         <div className="inputBox">
-          <input type="text" autoComplete="off" required />
+          <input
+            type="text"
+            autoComplete="off"
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
           <label>Firstname</label>
         </div>
         <div className="inputBox">
-          <input type="text" autoComplete="off" required />
+          <input
+            type="text"
+            autoComplete="off"
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
           <label>Lastname</label>
         </div>
         <div className="inputBox">
-          <input type="text" autoComplete="off" required />
+          <input
+            type="text"
+            autoComplete="off"
+            onChange={(e) => setUserName(e.target.value)}
+            required
+          />
           <label>Username</label>
         </div>
         <div className="inputBox">
-          <input type="email" autoComplete="off" required />
+          <input
+            type="email"
+            autoComplete="off"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
           <label>Email</label>
         </div>
         <div className="inputBox">
-          <input type="password" autoComplete="off" required />
+          <input
+            type="password"
+            autoComplete="off"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
           <label>Password</label>
         </div>
 
