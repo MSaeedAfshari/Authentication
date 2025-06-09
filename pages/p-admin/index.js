7ce917528e48ce3cfbe3f8713eaa539a9f8273d5
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-06-09 22:19:06 +0330

    add admin panel verification

--- pages/p-admin/index.js
+++ pages/p-admin/index.js
@@ -1,7 +1,54 @@
 import React from "react";
+import connecttoDB from "@/configs/db";
+import userModel from "@/models/User";
+import { verifyToken } from "@/utils/auth";
 
-function PAdmin() {
-  return <h1>Welcome To Admin Panel ❤️</h1>;
+function PAdmin({ user }) {
+  return <h1>Welcome To Admin Panel ❤️, Mr.{user.lastname}</h1>;
+}
+
+export async function getServerSideProps(context) {
+  const { token } = context.req.cookies;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/signin",
+      },
+    };
+  }
+
+  const isValidToken = await verifyToken(token);
+
+  if (!isValidToken) {
+    return {
+      redirect: {
+        destination: "/signin",
+      },
+    };
+  }
+  connecttoDB();
+
+  const user = await userModel.findOne(
+    {
+      email: isValidToken.email,
+    },
+    "firstname lastname role"
+  );
+
+  if (user.role !== "ADMIN") {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+
+  return {
+    props: {
+      user: JSON.parse(JSON.stringify(user)),
+    },
+  };
 }
 
 export default PAdmin;
