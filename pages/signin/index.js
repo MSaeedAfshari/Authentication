ad9e54bade3849c6760cd0b84b70115772002e58
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-06-09 15:45:03 +0330

    add login process

--- pages/signin/index.js
+++ pages/signin/index.js
@@ -1,20 +1,69 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 function Index() {
+  const [identifier, setIdentifier] = useState("");
+  const [password, setPassword] = useState("");
+
+  const route = useRouter();
+
+  const signIn = async (event) => {
+    event.preventDefault();
+
+    const user = { identifier, password };
+
+    const res = await fetch("/api/auth/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    console.log(res);
+    if (res.status === 200) {
+      setIdentifier("");
+      setPassword("");
+      alert("Logged in successfully");
+      route.replace("/dashboard");
+    } else if (res.status === 404) {
+      alert("User not found");
+    } else if (res.status === 422) {
+      alert("Username or password is incorrect");
+    } else {
+      alert("An error occured please try again later");
+    }
+  };
   return (
     <div className="box">
       <h1 align="center">Login Form</h1>
       <form role="form" method="post">
         <div className="inputBox">
-          <input type="text" autoComplete="off" required />
-          <label>Username</label>
+          <input
+            type="text"
+            autoComplete="off"
+            value={identifier}
+            onChange={(e) => setIdentifier(e.target.value)}
+            required
+          />
+          <label>Username | Email</label>
         </div>
         <div className="inputBox">
-          <input type="password" autoComplete="off" required />
+          <input
+            type="password"
+            autoComplete="off"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
           <label>Password</label>
         </div>
 
-        <input type="submit" className="register-btn" value="Sign In" />
+        <input
+          type="submit"
+          className="register-btn"
+          value="Sign In"
+          onClick={signIn}
+        />
       </form>
     </div>
   );
