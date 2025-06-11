4feb0131fe90c2cd133f9197a464fd85f41ab0ac
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-06-11 19:26:21 +0330

    code refactor

--- pages/p-admin/index.js
+++ pages/p-admin/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import connecttoDB from "@/configs/db";
 import userModel from "@/models/User";
+import { getSession } from "next-auth/react";
 
 function PAdmin({ user }) {
   return <h1>Welcome To Admin Panel ❤️, Mr.{user.lastname}</h1>;
 }
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps({ req }) {
   const session = await getSession({ req });
 
   console.log(session);
@@ -24,7 +25,7 @@ export async function getServerSideProps(context) {
     {
       email: session.user.email,
     },
-    "firstname lastname"
+    "firstname lastname role"
   );
 
   if (user.role !== "ADMIN") {
